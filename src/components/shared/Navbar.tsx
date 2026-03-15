e37af6bdd71fc21756a2dfd2e73f7782de76097c
Shaiket Mitra <shaiket.mitra21@gmail.com>
2026-03-15 12:03:01 +0600

    Changed Navbar Height and no Slider Loding

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -87,7 +87,7 @@ const Navbar = () => {
       className="w-full bg-gradient-to-r from-pink-600 to-pink-400 sticky top-0 z-50 shadow-lg"
     >
       <div className="px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 max-w-7xl mx-auto">
-        <div className="flex items-center justify-between h-[4.7rem]">
+        <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center">
             <motion.span
